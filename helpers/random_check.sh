716deb90611f985bfa9e9ec6a17d21784c071859
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2018-10-19 16:12:14 +0200

    make shellcheck happy

--- helpers/random_check.sh
+++ helpers/random_check.sh
@@ -8,17 +8,17 @@ then
 fi
 
 VERIFY_DIR=$(mktemp -d)
-cd $VERIFY_DIR 
+cd "$VERIFY_DIR" || exit
 echo "extracting files from backup (this will take some time)"
 borg list --format "{type} {path}{NL}" "$1" | grep -e "^-" | shuf -n 100 | grep -o "home/.*" | xargs -d '\n' borg extract "$1"
-IDENTICAL=$(diff -rs $VERIFY_DIR/home/nchiapol/ ~/ | grep -e 'identical$' | wc -l)
+IDENTICAL=$(diff -rs "$VERIFY_DIR/home/nchiapol/" ~/ | grep -ce 'identical$')
 
 if [ "$IDENTICAL" -eq 100 ]
 then
     echo -e "\nSUCCESS: all files are identical\n"
-    cd - >> /dev/null
+    cd - >> /dev/null || exit
     echo "removing temporary directory"
-    rm -rf $VERIFY_DIR
+    rm -rf "$VERIFY_DIR"
 else
     echo -e "\nFAILURE: $IDENTICAL/100 files are identical\n"
     echo "temporary directory not removed: $VERIFY_DIR"
