e00bc37bb9db7cbd30a2550c3925842d99650ee7
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2026-08-16 22:25:27 +0200

    add check for empty files in mail backups

--- helpers/unpack_archive.py
+++ helpers/unpack_archive.py
@@ -17,37 +17,60 @@ import os
 import sys
 import glob
 import libarchive
+import subprocess
 
 BASE_PATH = "/home/nchiapol/.mail-backup/"
 
-try:
-    account = sys.argv[1]
-except IndexError:
-    print("Usage:\n  unpack_archive.py <account>")
-    sys.exit(1)
+def check_for_empty(location):
+    """ check the backup folder for empty files
 
-archive = glob.glob(os.path.join(BASE_PATH, f"archives/Archive_{account}_*"))[0]
+    It seems akonadi/kmail sometimes looses mail-content
+    and just presists empty files. To spot such cases,
+    this function checks a backup location for empty files.
+    The path to empty files is printed to stdout.
+    (and mailed to the user if this script is exected from cron)
+    """
+    # if you have fdfind installed you could use this command as well
+    # command = ['fdfind', '.', location, '-uS', '-200b']
+    command = ['find', location, '-size', '-200c']
+    res = subprocess.run(command, capture_output=True, check=True)
+    if res.stdout:
+        print("**Tiny files present!**")
+        print(res.stdout.decode())
+    return True
 
-# check if archive has been unpacked already
-date_file = os.path.join(BASE_PATH, f"date_{account}")
-try:
-    with open(date_file, "r") as f:
-        old_date = f.readline().strip()
-except FileNotFoundError:
-    old_date = "0000-00-00"
-new_date = archive.split("_")[-1].split(".")[0]
-if not old_date < new_date:
-    print(f"No new archive found for {account}")
-    sys.exit(0)
+def main(account):
+    archive = glob.glob(os.path.join(BASE_PATH, f"archives/Archive_{account}_*"))[0]
 
-target_dir = os.path.join(BASE_PATH, f"unpacked_{account}")
-try:
-    os.chdir(target_dir)
-except FileNotFoundError:
-    os.mkdir(target_dir)
-    os.chdir(target_dir)
-print(f"unpacking: {archive} to {target_dir}")
-libarchive.extract_file(archive)
+    # check if archive has been unpacked already
+    date_file = os.path.join(BASE_PATH, f"date_{account}")
+    try:
+        with open(date_file, "r") as f:
+            old_date = f.readline().strip()
+    except FileNotFoundError:
+        old_date = "0000-00-00"
+    new_date = archive.split("_")[-1].split(".")[0]
+    if not old_date < new_date:
+        print(f"No new archive found for {account}")
+        sys.exit(0)
 
-with open(date_file, "w") as f:
-    f.write(new_date + "\n")
+    target_dir = os.path.join(BASE_PATH, f"unpacked_{account}")
+    try:
+        os.chdir(target_dir)
+    except FileNotFoundError:
+        os.mkdir(target_dir)
+        os.chdir(target_dir)
+    print(f"unpacking: {archive} to {target_dir}")
+    libarchive.extract_file(archive)
+    check_for_empty(target_dir)
+
+    with open(date_file, "w") as f:
+        f.write(new_date + "\n")
+
+if __name__ == "__main__":
+    try:
+        account = sys.argv[1]
+    except IndexError:
+        print("Usage:\n  unpack_archive.py <account>")
+        sys.exit(1)
+    main(account)
